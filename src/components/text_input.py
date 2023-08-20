02fb45e624089ea5ed64fa97f9d0d42464e73e68
Henry Paremain <henry.paremain@gmail.com>
2023-08-20 16:05:55 +0100

    latest

--- src/components/text_input.py
+++ src/components/text_input.py
@@ -1,7 +1,17 @@
-from dash import Dash, dcc, html, Input, Output,callback
+from dash import Dash, dcc, html, Input, Output,callback, dash_table
 from src.components import ids
 
 def render(app):
     text_field = html.Div([
-    dcc.Input(id='text-input', type='text', value='Initial text'),
-    html.Div(id=ids.NEWS_DATES)])
+    dcc.Input(id='text-input', type='text', value='Please enter company name'),
+    html.Div(id=ids.NEWS_COMPANY),
+    dash_table.DateTable(id='output-data-table')
+    ]
+    )
+    
+
+    @app.callback(
+        Output('output-data-table','data'),
+        Input(ids.NEWS_COMPANY,'value'))
+    
+    def show_df(va)
