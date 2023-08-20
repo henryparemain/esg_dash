9695a885769dc6be66297ea05c259a4630758b58
Henry Paremain <henry.paremain@gmail.com>
2023-08-20 16:09:09 +0100

    test2  new feature to be merged.

--- src/components/text_input.py
+++ src/components/text_input.py
@@ -5,3 +5,9 @@ def render(app):
     text_field = html.Div([
     dcc.Input(id='text-input', type='text', value='Initial text'),
     html.Div(id=ids.NEWS_DATES)])
+
+
+    this is a random test
+    lets pretend this is a new feature and I am pushing it to the repo to be merged. I will then go back to the other branch and try compare the differences 
+between files using this new extension. 
+
